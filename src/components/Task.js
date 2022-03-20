8406746d43bef46e865734bff30053dce1feee66
Jarod Wilson <jaroddwilson@gmail.com>
2022-03-20 12:09:30 -0400

    List displays and delete button works

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Task() {
+function Task({ task, onDelete }) {
+
+  function handleDelete(event) {
+    const target = event.target.id;
+    onDelete(target);
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.category}</div>
+      <div className="text">{task.text}</div>
+      <button className="delete" id={task.text} onClick={handleDelete}>X</button>
     </div>
   );
 }
